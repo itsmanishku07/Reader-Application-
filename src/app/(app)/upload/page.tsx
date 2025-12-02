a3b7138df787d6fc51c5bad136e16a627b9e9bad
Manish Kumar <manish0704ku@gmail.com>
2025-12-02 11:49:44 +0000

    in /upload getting page-40c9e11c7c46e465.js:1 Uncaught TypeError: (0 , s

--- src/app/(app)/upload/page.tsx
+++ src/app/(app)/upload/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { useActionState, useFormStatus } from "react-dom";
+import { useActionState } from "react";
+import { useFormStatus } from "react-dom";
 import { createBookAction } from "../actions";
 import { Header } from "@/components/layout/Header";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
